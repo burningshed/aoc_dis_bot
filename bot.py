651d96d27c3c7b349aa1336af63d66ea504eea6e
burningshed <39543956+burningshed@users.noreply.github.com>
2022-07-03 13:07:06 -0400

    retool advent of code bot for music league

--- bot.py
+++ bot.py
@@ -3,41 +3,38 @@ import requests
 import json
 from discord.ext import commands
 import discord
-from autoloader import Autoloader
+from MusicLeague import musicleague
 
 env_file = open("./.dis_config", 'rb')
-aoc_url = 'https://adventofcode.com/'
-current_year = '2020'
 env = pickle.load(env_file)
 env_file.close()
 
+ml = musicleague()
+
 bot = commands.Bot(command_prefix='!')
 
-al = Autoloader()
-al.connect()
-hbreak = "-" * 24
-allowed_cats = ('stars', 'local_score', 'global_score')
-allowed_years = ['20'+str(x) for x in range(15, 21)]
-@bot.command(name='sb',  help='prints the given years advent of code private scoreboard')
-async def score_board(ctx, year=current_year, cat='stars'):
-    if year not in allowed_years:
-        year = '2020'
-    if cat not in allowed_cats:
-        cats = 'stars'
-    lb = al.check_leaderboard(year)
+@bot.command(name='list',  help='prints the given years advent of code private scoreboard')
+async def list_themes(ctx):
+    theme_list = ml.get_cur_themes()
     title = ('name', 'stars')
+    hbreak = str("~"*12)
     response = \
     f"""```
-    {year} Advent of Code Leaderboard \n
+    Ryan (Still) Sucks Theme Selection List \n
     {hbreak}
-    {title[0]: ^16} | {cat}
+    ID | {title[0]: ^16} | "votes"
     {hbreak}
     """
-    for id_num in lb:
-        response += f"{lb[id_num]['name']: ^16} | {str(lb[id_num][cat])}\n\t"
+    for theme in theme_list:
+        response += f"{theme['id']} | {theme['name']: ^16} | {theme.get_votes()}\n\t"
     response += """
     ```"""
     await ctx.send(response)
 
 
 bot.run(env["DISCORD_TOKEN"])
+
+# TODO: create musicleague class, in it have accessable id and name objects and a get_votes class and get_cur_themes
+# 1. id
+# 2. name
+# 3. theme 
